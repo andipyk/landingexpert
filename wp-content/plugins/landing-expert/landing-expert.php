37fdaf8c6e6fc56027db758095c39a55b9a465dd
Andi Syafrianda <andisyafrianda@gmail.com>
2024-08-27 05:13:32 +0700

    git ignore

---
+++ wp-content/plugins/landing-expert/landing-expert.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * Plugin Name:       Landing Expert
+ * Plugin URI:        https://landingexpert.online
+ * Description:       This is a Udemy Plus plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.9
+ * Requires PHP:      8.0
+ * Author:            Andi Syafrianda
+ * Author URI:        https://instagram.com/andisyafrianda
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       landing-expert
+ * Domain Path:       /languages
+ */
+
+ if (!function_exists('add_action')) {
+    echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+    exit;
+ }
